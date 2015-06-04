983a86672f529e317461a12144177d44b11f2f9c
Nadya Malakhova <strettos@yandex.ru>
2015-06-04 21:56:04 +0400

    test dependent destroy & migration fix

--- db/migrate/20150602192047_create_answers.rb
+++ db/migrate/20150602192047_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
+      t.belongs_to :question, index: true
       t.text :body
-
       t.timestamps
     end
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe Question, :type => :model do
 
   it { should validate_presence_of :body }
 
-  it { should have_many :answers }
+  it { should have_many(:answers).dependent(:destroy) }
 end
